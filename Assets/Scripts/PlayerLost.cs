aed177b8806196cd95453b3b7449b87f6f87eac3
LukasCaha <lukas.caha@outlook.com>
2021-04-23 21:04:46 +0200

    GUI + file organisation update - organised files - added game over screen - added graphics to win screen - added inbetween level small win screen

---
+++ Assets/Scripts/PlayerLost.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLost : MonoBehaviour
+{
+    void Update()
+    {
+        if (this.transform.childCount <= 0)
+        {
+            //reset progress
+            PlayerPrefs.SetString("units", "");
+            PlayerPrefs.SetString("lastLevel", "");
+
+
+            SceneManager.LoadScene("Game over");
+        }
+    }
+}
---
+++ Assets/Scripts/SetParentActiveFalse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetParentActiveFalse : MonoBehaviour
+{
+    public void TurnParentOff()
+    {
+        this.gameObject.transform.parent.gameObject.SetActive(false);
+    }
+}
