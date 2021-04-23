e93944ae3f56ef9df89d033f6212adcb72b22b80
LukasCaha <lukas.caha@outlook.com>
2021-04-23 22:03:12 +0200

    Pause game

---
+++ Assets/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static GameObject panel = null;
+
+    public static float OriginalTimeScale = 1;
+
+    public bool IAmPanel;
+
+    private void Start()
+    {
+        if (IAmPanel)
+        {
+            Pause.panel = this.gameObject;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (Time.timeScale == 0)
+        {
+            Pause.panel.SetActive(false);
+            Time.timeScale = Pause.OriginalTimeScale;
+        }
+        else
+        {
+            Pause.panel.SetActive(true);
+            Pause.OriginalTimeScale = Time.timeScale;
+            Time.timeScale = 0;
+        }
+    }
+}
